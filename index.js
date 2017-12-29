833aea7b4ff1f6e42e854965c9bceb4513d5a381
Jeremy Fairbank <elpapapollo@gmail.com>
2017-12-29 17:53:53 -0500

    Use env PORT if available

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
+const PORT = process.env.PORT || 5000
+
 const app = express()
 
 app.use(express.static('build'))
 
-app.listen(5000, () => {
-  console.log('Server ready')
+app.listen(PORT, () => {
+  console.log(`Server ready on port ${PORT}`)
 })
