b7785f712508278570cfbc14d86089e8db114c92
Jeremy Fairbank <elpapapollo@gmail.com>
2017-12-30 14:42:12 -0500

    Add feed api to server

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const cors = require('cors')
+const db = require('./db.json')
+
+// Constants
+// =========
 
 const PORT = process.env.PORT || 5000
+const isProduction = process.env.NODE_ENV === 'production'
+
+// Helpers
+// =======
+
+const render = data => (req, res) => res.send(data)
+
+// API
+// ===
 
 const app = express()
 
+app.use(cors())
 app.use(express.static('build'))
 
+app.get('/badfeed', render(db.badfeed))
+app.get('/feed', render(db.feed))
+
 app.listen(PORT, () => {
-  console.log(`Server ready on port ${PORT}`)
+  if (!isProduction) {
+    console.log(`Server listening at http://localhost:${PORT}`)
+  }
 })
